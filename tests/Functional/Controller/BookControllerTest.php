d32a9566972ad890d29c18a63d980423eed2487c
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2020-08-30 23:03:51 +0200

    fix

--- tests/Functional/Controller/BookControllerTest.php
+++ tests/Functional/Controller/BookControllerTest.php
@@ -19,12 +19,12 @@ class BookControllerTest extends WebTestCase
        $crawler = $client->request('GET', '/books-list?page=2');
 
        $pages = $crawler->filter('.pagination, .current')->each(function ($node) {
-    	        return [
+    	 return [
       	    		'url' => $node->attr('href'),
       	    		'numPage' => trim($node->text()),
       	    		'html' => trim($node->html()),
     	    	  ];
-	        });
+	     });
 
 
        $numPage = $pages[1]['numPage'];
